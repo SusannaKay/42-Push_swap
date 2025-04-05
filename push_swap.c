031c884810425f3d6fe1de38b571fb190db6d80e
Susanna Kayed <skayed@e3r2p6.42roma.it>
2025-04-05 12:25:22 +0200

    Add: exit function

--- push_swap.c
+++ push_swap.c
@@ -6,30 +6,43 @@
 /*   By: skayed <skayed@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 01:47:02 by skayed            #+#    #+#             */
-/*   Updated: 2025/04/04 17:33:31 by skayed           ###   ########.fr       */
+/*   Updated: 2025/04/05 12:24:58 by skayed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-int main(int argc, char *argv[])
+
+int	free_all(t_node **a, t_node **b, int *array, char *error)
 {
-	t_node *a;
-	t_node *b;
-	int *array;
-	int size;
-	
+	if (error)
+		ft_printf(error);
+	if (a && *a)
+		free_stack(a);
+	if (b && *b)
+		free_stack(b);
+	if (array)
+		free(array);
+	return(1);
+}
+int	main(int argc, char *argv[])
+{
+	t_node	*a;
+	t_node	*b;
+	int		*array;
+	int		size;
+
 	b = NULL;
 	if (argc < 2)
-		return(0);
+		return (0);
 	array = parse_prompt(argc, argv, array, &size);
 	if (!array)
-		return(ft_printf("Error\n"), 1);
-	if(!sort_array(array, size))
-		{free(array);
-		return(ft_printf("Error\n"), 1);}
+		return (ft_printf("Error\n"), 1);
+	if (!check_array(array, size))
+	{
+		free(array);
+		return (ft_printf("Error\n"), 1);
+	}
 	fill_stack(a, array, size);
-	
 	free(array);
 	return (0);
 }
-// funzione uscita e libera nodi 
--- push_swap.h
+++ push_swap.h
@@ -6,14 +6,14 @@
 /*   By: skayed <skayed@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 01:51:15 by skayed            #+#    #+#             */
-/*   Updated: 2025/04/04 17:33:11 by skayed           ###   ########.fr       */
+/*   Updated: 2025/04/05 12:24:48 by skayed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <stdlib.h>
+# include <stdlib.h>
 typedef struct s_node
 {
 	int				value;
@@ -32,9 +32,9 @@ void				rr(t_node **a, t_node **b);
 void				rra(t_node **a);
 void				rrb(t_node **b);
 void				rrr(t_node **a, t_node **b);
-int	*parse_prompt(int argc, char **argv, int *array, int *size);
-int sort_array(int *array, int size);
-void	fill_stack(t_node **a, int *array, int size);
-
-
+int					*parse_prompt(int argc, char **argv, int *array, int *size);
+int					check_array(int *array, int size);
+void				fill_stack(t_node **a, int *array, int size);
+int					*bubble_sort(int *array, int size);
+int					free_all(t_node **a, t_node **b, int *array, char *error)
 #endif
