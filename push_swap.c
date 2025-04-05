3c499cf04fe670f6f2e22e90eaaf7e04a92ab98c
Susanna kayed <skayed@MacBook-Pro-di-Susanna.local>
2025-04-05 08:17:17 +0200

    cleaned code

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: skayed <skayed@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 01:47:02 by skayed            #+#    #+#             */
-/*   Updated: 2025/04/02 16:55:18 by skayed           ###   ########.fr       */
+/*   Updated: 2025/04/04 17:33:31 by skayed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@ int main(int argc, char *argv[])
 {
 	t_node *a;
 	t_node *b;
+	int *array;
+	int size;
 	
 	b = NULL;
+	if (argc < 2)
+		return(0);
+	array = parse_prompt(argc, argv, array, &size);
+	if (!array)
+		return(ft_printf("Error\n"), 1);
+	if(!sort_array(array, size))
+		{free(array);
+		return(ft_printf("Error\n"), 1);}
+	fill_stack(a, array, size);
 	
-	if (argc == 2)
-	{
-		a = validate_prompt(argv[1]);
-		if (!a)
-		{
-			ft_printf("Error\n");
-			return(1);
-		}
-	}
-	else
-	{
-		
-	}
-	
+	free(array);
+	return (0);
 }
+// funzione uscita e libera nodi 
